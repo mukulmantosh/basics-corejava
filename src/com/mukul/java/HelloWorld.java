66fd09cd17d0d8ac2c538532be7369030c552d9d
Mukul Mantosh <mukulmantosh@yahoo.com>
2016-12-28 11:43:24 +0530

    Created Java Program HelloWorld

---
+++ src/com/mukul/java/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.mukul.java;
+
+public class HelloWorld {
+
+    public void print(){
+        System.out.println("Hello World");
+    }
+}
--- src/com/mukul/java/Main.java
+++ src/com/mukul/java/Main.java
@@ -3,7 +3,11 @@ package com.mukul.java;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Hello World");
+
+        HelloWorld HW = new HelloWorld(); //Creating Hello World Object
+        HW.print(); // Calling Method print inside class Hello World
+
+
 
     }
 }
